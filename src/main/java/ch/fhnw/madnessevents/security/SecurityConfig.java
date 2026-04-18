ee681c587176aa62db4e25782dc5f197e855663a
Ajmal-shafaq <ajmal.shafaq@gmail.com>
2026-04-18 12:37:57 +0200

    Initial MadnessEvents backend setup

---
+++ src/main/java/ch/fhnw/madnessevents/security/SecurityConfig.java
@@ -0,0 +1,60 @@
+package ch.fhnw.madnessevents.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+   @Bean
+public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers(
+                            "/swagger-ui.html",
+                            "/swagger-ui/**",
+                            "/v3/api-docs/**",
+                            "/h2-console/**",
+                            "/hello",
+                            "/error"
+                    ).permitAll()
+
+                    .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN")
+                    .requestMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN")
+                    .requestMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN")
+
+                    .anyRequest().authenticated()
+            )
+            .httpBasic(Customizer.withDefaults());
+
+    http.headers(headers -> headers.frameOptions(frame -> frame.disable()));
+
+    return http.build();
+}
+@Bean
+public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+    UserDetails admin = User.withUsername("admin")
+            .password(passwordEncoder.encode("admin123"))
+            .roles("ADMIN")
+            .build();
+
+    return new InMemoryUserDetailsManager(admin);
+}
+
+@Bean
+public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+}
+}
